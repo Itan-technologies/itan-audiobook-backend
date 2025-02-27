f6e1eb796b9da8f6ba4effdfead0d16ca144209b
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-27 12:46:16 +0100

    Create admin sessions for sign in and sign out

---
+++ app/controllers/admins/sessions_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Admins::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  def set_flash_message(key, kind, options = {})
+    # Do nothing as flash is not available in API-only apps
+  end
+
+  def set_flash_message!(key, kind, options = {})
+    # Do nothing as flash is not available in API-only apps
+  end
+    
+  private
+  
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: 'Admin logged in successfully.' },
+        data: AdminSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: { code: 401, message: "Admin couldn't be found." }
+      }, status: :unauthorized
+    end
+  end
+  
+  def respond_to_on_destroy
+    if current_admin
+      render json: {
+        status: 200,
+        message: "Admin logged out successfully."
+      }
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
+  end
+  
+end
