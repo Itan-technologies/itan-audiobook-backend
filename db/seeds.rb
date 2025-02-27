473d18121f071b14f1085ff12409a4b2a301676b
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-27 12:49:11 +0100

    Create admin in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Admin.create!(
+  email: 'admin@example.com',
+  password: 'password123@1',
+  password_confirmation: 'password123@1'
+)
+
+puts 'Admin seeded'
