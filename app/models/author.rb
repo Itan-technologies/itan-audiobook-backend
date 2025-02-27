fbad40eb2375bfb0710f906debd036a33a6b3bb5
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-27 13:50:29 +0100

    Fix linter errors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
-self.primary_key = :id
+  self.primary_key = :id
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
