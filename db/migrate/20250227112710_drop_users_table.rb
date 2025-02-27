8d4666bb5ec68d89c0c854b17f889906afb2da14
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-27 12:48:53 +0100

    Remove user's table

---
+++ db/migrate/20250227112710_drop_users_table.rb
@@ -0,0 +1,19 @@
+class DropUsersTable < ActiveRecord::Migration[7.1]
+  def up
+    drop_table :users
+  end
+
+  def down
+    create_table :users do |t|
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.timestamps null: false
+      t.integer "role"
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
+  end
+end
