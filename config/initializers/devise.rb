aa62a3d0b0519307900dfd64ddf78230dd9f6ba4
Ol-create <oluola96@gmail.com>
2025-03-04 14:05:36 +0100

    Tell Devise to generate email confirmation links pointing to your Next.js frontend

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,14 +24,16 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'itan@gmail.com'
+
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'AuthorMailer'
 
   # Configure the parent class responsible to send e-mails.
   config.parent_mailer = 'ActionMailer::Base'
-
+  # Tell Devise to generate email confirmation links pointing to your Next.js frontend
+config.confirmation_url = ENV['EMAIL_CONFIRMATION_URL']
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
