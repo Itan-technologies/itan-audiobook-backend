9904536a7607a66c96c9334317dfb3a57702ce04
Ayokunnumi1 <omololuayk@gmail.com>
2025-03-18 23:40:34 +0100

    Update active storage service to use local storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,22 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon
-  config.active_storage.direct_upload = true
+  config.active_storage.service = :local
+  
+  config.active_storage.direct_upload = false
+
+  # Ensure ActiveStorage has its own URL options
+  # config.after_initialize do
+  #   ActiveStorage::Current.url_options = {
+  #     host: "localhost:3000",
+  #     protocol: "http"
+  #   }
+  # end
+
+  # Rails.application.routes.default_url_options = {
+  #   host: "localhost:3000",
+  #   protocol: "http"
+  # }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
