8922f56d53b62acfa2701e8af4697b2ebb9fab14
Ayokunnumi1 <omololuayk@gmail.com>
2025-03-20 18:09:11 +0100

    Update config in production for url generation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,10 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
+
   config.active_storage.direct_upload = true
+
+  Rails.application.routes.default_url_options[:host] = 'your-production-domain.com'
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
