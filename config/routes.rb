080bb04087db31b10ad7c5bfb35a00b1b147443f
Ayokunnumi1 <omololuayk@gmail.com>
2025-03-25 14:29:48 +0100

    Update routes for authors profile

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
 
   post '/direct_uploads', to: 'direct_uploads#create'
 
+  namespace :authors do
+    resource :profile, only: [:show, :update, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
