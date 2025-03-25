67dc1788505406587333c86dae2a864fd7ef8f82
Ayokunnumi1 <omololuayk@gmail.com>
2025-03-25 14:29:32 +0100

    Update author serializer to accomodate profile attributes

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -1,5 +1,6 @@
 class AuthorSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :email, :created_at
+  attributes :id, :email, :created_at, :first_name, :last_name, 
+             :bio, :phone_number, :country, :location
 end
