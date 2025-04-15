e48a4d10c94c2042600fdf82c0736283fbd2e1f3
Ayokunnumi1 <omololuayk@gmail.com>
2025-04-15 08:35:08 +0100

    Update author mailer with reset password & verification code actions

--- app/mailers/author_mailer.rb
+++ app/mailers/author_mailer.rb
@@ -4,7 +4,18 @@ class AuthorMailer < Devise::Mailer
   default from: 'omololuayk@gmail.com'
 
   def confirmation_instructions(record, token, opts = {})
-    @confirmation_url = "http://localhost:3000/authors/confirmation?confirmation_token=#{token}"
+    @confirmation_url = "http://localhost:9000/authors/confirmation?confirmation_token=#{token}"
     super
+  end
+
+  def reset_password_instructions(record, token, opts = {})
+    @reset_password_url = "http://localhost:9000/reset-password?reset_password_token=#{token}"
+    super
+  end
+
+  def verification_code(author, code)
+    @author = author
+    @code = code
+    mail(to: author.email, subject: "Your Login Verification Code")
   end
 end
