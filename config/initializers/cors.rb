4cb1505cbe6a8a44b8c16238b2d4551a0b911ef2
Ayokunnumi1 <omololuayk@gmail.com>
2025-04-25 14:29:00 +0100

    Update CORS to add new method for exposing authorization token to frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,6 +19,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],      
       credentials: true
   end
 end
