b9d693d7f26701eaf9e338c7a5168c4a34f95522
Ayokunnumi1 <omololuayk@gmail.com>
2025-05-01 23:48:20 +0100

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,11 @@ Rails.application.routes.draw do
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  devise_scope :author do
+    get '/confirmation', to: 'api/v1/authors/confirmations#show'
+  end
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -74,7 +78,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "api/v1/status#index"
-
-  # Direct route for confirmation links in emails
-  get '/confirmation', to: 'api/v1/authors/confirmations#show'
 end
