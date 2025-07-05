b01f4eeac19f2ecebf7167b4f06a133d776befc7
Ayokunnumi1 <omololuayk@gmail.com>
2025-07-05 23:04:09 +0100

    Update likes controller

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,6 +1,27 @@
 class Api::V1::LikesController < ApplicationController
     before_action :authenticate_reader!
   
+    def index
+      likes = current_reader.likes.includes(book: { cover_image_attachment: :blob }).order(created_at: :desc)
+      render json: {
+        status: { code: 200 },
+        data: likes.map do |like|
+          {
+            id: like.id,
+            book: {
+              id: like.book.id,
+              title: like.book.title,
+              author: "#{like.book.author.first_name} #{like.book.author.last_name}",
+              cover_image_url: (
+                Rails.application.routes.url_helpers.url_for(like.book.cover_image) if like.book.cover_image.attached?
+              )
+            },
+            liked_at: like.created_at
+          }
+        end
+      }
+    end
+    
     def create
       like = Like.new(book_id: params[:book_id], reader_id: current_reader.id)
       if like.save
